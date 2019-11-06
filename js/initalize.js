8e187111b777040e897231965dd20a5c28b83bf4
Ypig <43112997+Ypig@users.noreply.github.com>
2019-11-06 21:27:38 +0800

    summary

--- js/initalize.js
+++ js/initalize.js
@@ -1,1 +1,5 @@
 var no_function_tip = new mdui.Dialog('#no_function_dialog');
+var menulist = new mdui.Menu('#open_menu', '#menu');
+document.getElementById('open_menu').addEventListener('click', function () {
+  menulist.open();
+});
