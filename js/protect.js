57a803ed1fadcf1c112052f058468b29d76ed830
Ypig <43112997+Ypig@users.noreply.github.com>
2019-11-08 18:31:52 +0800

    summary

--- js/protect.js
+++ js/protect.js
@@ -2,6 +2,7 @@ $.get("https://www.ypigapi.tk/getssr.php",function(data,status){
 
 
     if (status == "success") {
+      $('#link_list_load').remove();
       var links = jQuery.parseJSON(data);
       for (var i = 0; i < links.links.length; i++) {
         link_list[i] = links.links[i];
@@ -10,5 +11,7 @@ $.get("https://www.ypigapi.tk/getssr.php",function(data,status){
           $('<li class="mdui-list-item mdui-ripple"><i class="mdui-list-item-icon mdui-icon material-icons">send</i><div class="mdui-list-item-content" href="javascript:void(0);" onclick="location.href=\'https://apps.ypig.tk/p/'+ link_list[j] +'\'">' + links.name[j] + '</div></li>').appendTo("#link_list_show");
       }
       mdui.mutation();
+    } else {
+      $('<li class="mdui-list-item mdui-ripple"><i class="mdui-list-item-icon mdui-icon material-icons">error_outline</i><div class="mdui-list-item-content" href="javascript:void(0);">获取出错，刷新一下吧</div></li>').appendTo("#link_list_show");
     }
 });
