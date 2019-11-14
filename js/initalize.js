da8ba0e50cf9d781f4c6159d64d1ac3b45057f70
Ypig <43112997+Ypig@users.noreply.github.com>
2019-11-14 13:54:40 +0800

    summary

--- js/initalize.js
+++ js/initalize.js
@@ -3,6 +3,7 @@ try {
   var no_function_tip = new mdui.Dialog('#no_function_dialog');
   var do_feedback_tip = new mdui.Dialog('#do_feedback_dialog');
   var copy_success_tip = new mdui.Dialog('#copy_success_dialog');
+  var check_vip_tip = new mdui.Dialog('#check_vip_dialog');
   var menulist = new mdui.Menu('#open_menu', '#menu');
   document.getElementById('open_menu').addEventListener('click', function () {
     menulist.open();
